f2bb999d15626fea843ea1c931374cf54824e8d8
AyrtonG94 <ayrton.gomes@live.com>
2022-07-23 18:08:21 -0300

    removi o link que redireciona para as postagens

--- blog/resources/views/dashboard.blade.php
+++ blog/resources/views/dashboard.blade.php
@@ -9,8 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                <p style="color:blue">Você está logado! <p><br> <br>
-                <a href="{{url('/api/posts')}}">  Ver todas as postagens</a>
+                <p style="color:blue">Você está logado! <p>
                 </div>
             </div>
         </div>
